ccc0690b847f4c5d7dc04ec3918aa25c62d83083
Chris Continanza <christopher.continanza@gmail.com>
2010-12-12 14:12:49 -0600

    refactor and add bin for tweeting cables

--- lib/cablegator/version.rb
+++ lib/cablegator/version.rb
@@ -1,3 +1,3 @@
 module Cablegator
-  VERSION = "0.9.2"
+  VERSION = "0.9.3"
 end
--- lib/cablegator/wikileaks.rb
+++ lib/cablegator/wikileaks.rb
@@ -1,12 +1,22 @@
 class WikiLeaks 
   include HTTParty
   base_uri 'http://wikileaks.ch'
-
   HOME = '/cablegate.html'
 
   class << self
-    def home
-      self.get(HOME)
+    def reference_id(cable_url)
+      File.basename(cable_url).gsub(File.extname(cable_url),'') 
+    end
+
+    def with_each_cable
+      doc = Nokogiri::HTML(self.get(HOME))
+      doc.css(%{a[href^='/date']}).each do |link|
+        page_with_cables = Nokogiri::HTML(WikiLeaks.get(link.attributes['href'].value))
+        page_with_cables.css(%{a[href^='/cable']}).each do |cable|
+          cable_url = cable.attributes['href'].value
+          yield cable_url
+        end
+      end
     end
   end
 end
