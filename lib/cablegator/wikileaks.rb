8df33d21911c5c0f3edc4d4678990f84d0b43cf3
kory <kory@rogue.(none)>
2010-12-12 18:03:07 -0600

    Added method for getting subject and location data

--- lib/cablegator/wikileaks.rb
+++ lib/cablegator/wikileaks.rb
@@ -18,5 +18,25 @@ class WikiLeaks
         end
       end
     end
+
+    def with_each_cable_data
+      doc = Nokogiri::HTML(self.get(HOME))
+      doc.css(%{a[href^='/date']}).each do |link|
+        page_with_cables = Nokogiri::HTML(WikiLeaks.get(link.attributes['href'].value))
+
+        page_with_cables.css(%{table.cable tr:has(td)}).each do |cable|
+
+          cable_hash = {}	
+	  children = cable.element_children.map{ |x| x.text.strip }
+    	  cable_hash['reference_id'] = children[0]	  
+	  cable_hash['subject'] = children[1]	  
+	  cable_hash['origin_date'] =  children[2]	  
+	  cable_hash['release_date'] = children[3]	  
+	  cable_hash['classification'] = children[4]
+	  cable_hash['location'] = children[5]
+	  yield cable_hash
+        end
+      end
+    end
   end
 end
