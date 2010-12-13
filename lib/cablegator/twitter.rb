c291bcbe07c42921631d576061fa4f3e8ff29ee2
Chris Continanza <christopher.continanza@gmail.com>
2010-12-13 02:12:55 -0600

    adding tweet_cables_format

---
+++ lib/cablegator/twitter.rb
@@ -0,0 +1,34 @@
+class Twitter
+  def self.command_line_login
+    consumer_key = 'ddxhhHSHys7210VR8lhYag'
+    consumer_secret = 'FN5kIDAvwWU4jb54JMfWWTbtpI30JKeEmRWMYSMYk'
+
+    oauth_client = OAuth::Consumer.new(consumer_key, consumer_secret, 
+                        :site => 'http://api.twitter.com', 
+                        :request_endpoint => 'http://api.twitter.com', 
+                        :sign_in => true)
+
+    puts "Enter Twitter Username:"
+    username = STDIN.gets.strip 
+
+    req_token = oauth_client.get_request_token
+    Launchy.open(req_token.authorize_url)
+
+    puts "\nAllow access to get pin\n"
+
+    puts "Enter Pin: "
+    pin = STDIN.gets.strip 
+
+    access_token = req_token.get_access_token(:oauth_verifier => pin)
+
+    Twitter.configure do |config|
+      config.consumer_key = access_token.consumer.key
+      config.consumer_secret = access_token.consumer.secret
+      config.oauth_token = access_token.token 
+      config.oauth_token_secret = access_token.secret
+    end
+
+    Twitter.user(username)
+    username
+  end
+end
